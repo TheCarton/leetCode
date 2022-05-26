3d644a24752688228db407f406613763d0d7820e
Luke <vandermale.l@northeastern.edu>
2022-05-26 19:06:20 -0400

    progres????

--- medianOfTwoSortedArrays/median.py
+++ medianOfTwoSortedArrays/median.py
@@ -7,18 +7,39 @@ The overall run time complexity should be O(log (m+n))
 
 
 class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        list_of_medians = []
-        if len(nums1) > 0:
-            list_of_medians.append(self.find_median(nums1))
-        if len(nums2) > 0:
-            list_of_medians.append(self.find_median(nums2))
-        return self.find_median(list_of_medians)
+    def merge(self, nums1, nums2):
+
+        if not nums1 and not nums2:
+            return []
+        if not nums1:
+            return nums2
+        if not nums2:
+            return nums1
+
+        merged_array = []
+        n1_iter = iter(nums1)
+        n2_iter = iter(nums2)
+
+        n1 = next(n1_iter, None)
+        n2 = next(n2_iter, None)
+
+        while n1 is not None and n2 is not None:
+            if n1 <= n2:
+                merged_array.append(n1)
+                n1 = next(n1_iter, None)
+            else:
+                merged_array.append(n2)
+                n2 = next(n2_iter, None)
+
+        while n1 is not None:
+            merged_array.append(n1)
+            n1 = next(n1_iter, None)
+
+        while n2 is not None:
+            merged_array.append(n2)
+            n2 = next(n2_iter, None)
+
+        return merged_array
 
     def find_median(self, nums) -> float:
         if len(nums) == 1:
@@ -34,4 +55,149 @@ class Solution(object):
         else:
             return float(nums[middle_index])
 
+    def remove_min(self, nums1, nums2):
+        if not nums1 and not nums2:
+            return [], []
+        if not nums1:
+            nums2.pop(0)
+            return [], nums2
+        if not nums2:
+            nums1.pop(0)
+            return nums1, []
+        if nums1[0] <= nums2[0]:
+            nums1.pop(0)
+        else:
+            nums2.pop(0)
+        return nums1, nums2
 
+    def remove_max(self, nums1, nums2):
+        if not nums1 and not nums2:
+            return [], []
+        if not nums1:
+            nums2.pop()
+            return [], nums2
+        if not nums2:
+            nums1.pop()
+            return nums1, []
+        if nums1[-1] >= nums2[-1]:
+            nums1.pop()
+        else:
+            nums2.pop()
+        return nums1, nums2
+
+    def switch_number_to_smaller_list(self, small_list, big_list):
+        if big_list[0] >= small_list[-1]:
+            small_list.append(big_list.pop(0))
+            return small_list, big_list
+        else:
+            for i, n in enumerate(small_list):
+                if big_list[0] <= n:
+                    small_list.insert(i, big_list.pop(0))
+                    break
+        return small_list, big_list
+
+    def order_lists(self, nums1, nums2):
+        if len(nums1) >= len(nums2):
+            (small_list, big_list) = nums2, nums1
+        else:
+            (small_list, big_list) = nums1, nums2
+        return small_list, big_list
+
+    def balance_large(self, nums1, nums2):
+        (small_list, big_list) = self.order_lists(nums1, nums2)
+        if len(small_list) == 0:
+            small_list = big_list[:len(big_list) // 2]
+            big_list = big_list[len(big_list) // 2:]
+            return self.order_lists(small_list, big_list)
+
+        i = len(big_list) // 2
+        while i < len(big_list):
+            if big_list[i] >= small_list[-1]:
+                tail = big_list[i:]
+                small_list = small_list + tail
+                big_list = big_list[:i]
+                break
+            i += 1
+        (small_list, big_list) = self.order_lists(small_list, big_list)
+        j = len(big_list) // 2
+        while j > 0:
+            if big_list[j] <= small_list[0]:
+                head = big_list[:j]
+                small_list = head + small_list
+                big_list = big_list[j:]
+                break
+            j -= 1
+        (small_list, big_list) = self.order_lists(small_list, big_list)
+        return small_list, big_list
+
+    def balance_lists(self, nums1, nums2):
+        (small_list, big_list) = self.balance_large(nums1, nums2)
+        while len(big_list) != len(small_list):
+            small_list, big_list = self.switch_number_to_smaller_list(small_list, big_list)
+        return self.order_lists(small_list, big_list)
+
+    def prepare_lists(self, nums1, nums2):
+        split_lists_are_even = ((len(nums1) + len(nums2)) / 2) % 2 == 0
+        split_lists_unequal = (len(nums1) + len(nums2)) / 2 != (len(nums1) + len(nums2)) // 2
+        merged_list_gt_four = (len(nums1) + len(nums2)) > 4
+        while split_lists_are_even or split_lists_unequal and merged_list_gt_four:
+            nums1, nums2 = self.remove_min(nums1, nums2)
+            nums1, nums2 = self.remove_max(nums1, nums2)
+            split_lists_are_even = ((len(nums1) + len(nums2)) / 2) % 2 == 0
+            split_lists_unequal = (len(nums1) + len(nums2)) / 2 != (len(nums1) + len(nums2)) // 2
+            merged_list_gt_four = (len(nums1) + len(nums2)) > 4
+
+        (small_list, big_list) = self.order_lists(nums1, nums2)
+
+        if not merged_list_gt_four:
+            return small_list, big_list
+
+        else:
+            return self.balance_lists(small_list, big_list)
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        at_least_one_is_empty = not (nums1 and nums2)
+        if at_least_one_is_empty or (len(nums1) == 1 and len(nums2) == 1):
+            return self.find_median(nums1 + nums2)
+
+        # If we get here, the lists are 'mixed' and non-empty,
+        # so we can't just union them.
+
+        if len(nums1) + len(nums2) < 5:
+            return self.find_median(self.merge(nums1, nums2))
+
+        if self.find_median(nums1) == self.find_median(nums2):
+            return self.find_median(nums1)
+
+        total_len_is_even = (len(nums1) + len(nums2)) % 2 == 0
+        nums1_is_even = len(nums1) % 2 == 0
+        nums2_is_even = len(nums2) % 2 == 0
+
+        if nums1_is_even or nums2_is_even or len(nums1) != len(nums2):
+            nums1, nums2 = self.prepare_lists(nums1, nums2)
+
+        # Here, nums1 and nums2 should both be odd and have equal lengths.
+
+        while len(nums1) > 2 and len(nums2) > 2:
+            nums1_median = self.find_median(nums1)
+            median_index = len(nums1) // 2
+            nums2_median = self.find_median(nums2)
+            if nums1_median == nums2_median:
+                return nums1_median
+            if nums1_median < nums2_median:
+                nums1 = nums1[median_index:]
+                nums2 = nums2[:median_index + 1]
+            if nums1_median > nums2_median:
+                nums1 = nums1[:median_index + 1]
+                nums2 = nums2[median_index:]
+        reduced_len_is_even = (len(nums1) + len(nums2)) % 2 == 0
+
+        if not total_len_is_even == reduced_len_is_even:
+            (nums1, nums2) = self.remove_min(nums1, nums2)
+        merged = self.merge(nums1, nums2)
+        return self.find_median(merged)
--- medianOfTwoSortedArrays/test_median.py
+++ medianOfTwoSortedArrays/test_median.py
@@ -3,6 +3,179 @@ import random
 import statistics
 
 
+def test_merge():
+    sol = median.Solution()
+    a = [1, 2]
+    b = [3]
+    assert sol.merge(a, b) == [1, 2, 3]
+
+    assert sol.merge([], []) == []
+
+    assert sol.merge(b, []) == b
+
+    assert sol.merge([], a) == a
+
+
+def test_merge_2():
+    sol = median.Solution()
+    assert sol.merge([3], [1, 2]) == [1, 2, 3]
+
+
+def test_order_lists():
+    sol = median.Solution()
+    assert sol.order_lists([1, 2, 3], []) == ([], [1, 2, 3])
+
+
+def test_order_lists_random():
+    sol = median.Solution()
+    for i in range(1000):
+        a_length = random.randint(0, 50)
+        b_length = random.randint(0, 50)
+        million = 1000000
+
+        a = random.sample(range(-million, million), a_length)
+        a.sort()
+        b = random.sample(range(-million, million), b_length)
+        b.sort()
+        (small_actual, big_actual) = sol.order_lists(a, b)
+        assert len(small_actual) <= len(big_actual)
+
+
+def test_balance_large_test():
+    sol = median.Solution()
+    a = [1, 2, 3]
+    b = [1, 4, 9, 11, 25, 63, 78, 100, 123, 196, 155]
+    actual_a, actual_b = sol.balance_large(a, b)
+    assert actual_a, actual_b == ([1, 2, 3, 63, 78, 100, 123, 196, 155], [1, 4, 9, 11, 25])
+
+
+def test_balance_large_test_big_median_lt():
+    sol = median.Solution()
+    a = [1, 256, 300]
+    b = [1, 4, 9, 11, 25, 63, 78, 100, 123, 196, 155]
+    actual_a, actual_b = sol.balance_large(a, b)
+    assert actual_a, actual_b == ([1, 4, 9, 11, 25, 256, 300], [78, 100, 123, 196, 155])
+
+
+def test_balance_large_random():
+    sol = median.Solution()
+    for i in range(50):
+        a_length = random.randint(3, 10)
+        b_length = random.randint(0, 10)
+        million = 1000000
+
+        a = random.sample(range(-10, 10), a_length)
+        a.sort()
+        b = random.sample(range(-10, 10), b_length)
+        b.sort()
+
+        expected_median = statistics.median(a + b)
+
+        actual_a, actual_b = sol.balance_large(a, b)
+        remaining_median = statistics.median(actual_a + actual_b)
+        succeed = (expected_median == remaining_median) and len(actual_a) * 2 >= len(actual_b)
+        if not succeed:
+            print(a, b)
+            print(actual_a, actual_b)
+            print("Expected median = {}".format(expected_median))
+            print("Remaining median = {}".format(remaining_median))
+        assert succeed
+
+
+def test_balance_large_from_random():
+    sol = median.Solution()
+    a = [-10, -4, -2, 3, 7]
+    b = [-8, -2]
+    expected_median = statistics.median(a + b)
+
+    actual_a, actual_b = sol.balance_large(a, b)
+    remaining_median = statistics.median(actual_a + actual_b)
+    assert remaining_median == expected_median
+    assert actual_a == sorted(actual_a)
+    assert actual_b == sorted(actual_b)
+    assert len(actual_a) * 3 >= len(actual_b)
+
+
+def test_balance_lists():
+    sol = median.Solution()
+    a = [-1, 10, 50]
+    b = [-200, -50, 0, 1, 7, 65, 100, 120, 121, 122, 156]
+    actual_a, actual_b = sol.balance_lists(a, b)
+    assert actual_a == sorted(actual_a)
+    assert actual_b == sorted(actual_b)
+    assert len(actual_a) == len(actual_b)
+
+
+def test_balance_lists_one_empty():
+    sol = median.Solution()
+    a = [-50, 72936, 72936, 107639, 107639, 132960]
+    b = []
+    actual_a, actual_b = sol.balance_lists(a, b)
+    assert actual_a == sorted(actual_a)
+    assert actual_b == sorted(actual_b)
+    assert len(actual_a) == len(actual_b)
+
+
+def test_prepare_lists():
+    sol = median.Solution()
+    a = [-1, 10, 50]
+    b = [-200, -50, 0, 1, 7, 65, 100, 120, 121, 122, 156, 569, 603]
+    actual_a, actual_b = sol.prepare_lists(a, b)
+    assert len(actual_a) % 2 != 0
+    assert len(actual_b) % 2 != 0
+    assert actual_a == sorted(actual_a)
+    assert actual_b == sorted(actual_b)
+    assert len(actual_a) == len(actual_b)
+
+
+def test_prepare_lists_random():
+    sol = median.Solution()
+    for i in range(50):
+        a_length = random.randint(3, 50)
+        b_length = random.randint(3, 50)
+        million = 1000000
+
+        a = random.sample(range(-million, million), a_length)
+        a.sort()
+        b = random.sample(range(-million, million), b_length)
+        b.sort()
+
+        actual_a, actual_b = sol.prepare_lists(a, b)
+        if len(actual_a) + len(actual_b) > 4:
+            assert len(actual_a) % 2 != 0
+            assert len(actual_b) % 2 != 0
+            assert actual_a == sorted(actual_a)
+            assert actual_b == sorted(actual_b)
+            assert len(actual_a) == len(actual_b)
+        else:
+            assert actual_a == sorted(actual_a)
+            assert actual_b == sorted(actual_b)
+
+
+def test_switch_number_small_lt_big():
+    sol = median.Solution()
+    small = [-5, 0, 1]
+    big = [2, 5, 6, 10, 15]
+    actual_a, actual_b = sol.switch_number_to_smaller_list(small, big)
+    assert actual_a, actual_b == ([-5, 0, 1, 15], [2, 5, 6, 10])
+
+
+def test_switch_number_small_gt_big():
+    sol = median.Solution()
+    small = [20, 30, 41]
+    big = [2, 5, 6, 10, 15]
+    actual_a, actual_b = sol.switch_number_to_smaller_list(small, big)
+    assert actual_a, actual_b == ([2, 20, 30, 41], [5, 6, 10])
+
+
+def test_switch_number_small_mid_big():
+    sol = median.Solution()
+    small = [6, 6.5, 7]
+    big = [2, 5, 6, 10, 15]
+    actual_a, actual_b = sol.switch_number_to_smaller_list(small, big)
+    assert actual_a, actual_b == ([6, 6, 6.5, 7], [2, 5, 10, 15])
+
+
 def test_find_median_singleton():
     sol = median.Solution()
     assert sol.find_median([1]) == 1
@@ -20,6 +193,31 @@ def test_find_median_even():
     assert sol.find_median([1, 3]) == 2
 
 
+def test_remove_min():
+    sol = median.Solution()
+    (a, b) = [-1, 0], [-50, 1]
+    sol.remove_min(a, b)
+    assert b == [1]
+    assert a == [-1, 0]
+    sol.remove_min(a, b)
+    assert b == [1]
+    assert a == [0]
+
+    sol.remove_min(a, b)
+    assert b == [1]
+    assert a == []
+
+
+def test_remove_max():
+    sol = median.Solution()
+    (a, b) = [1, 100], [-68, 10]
+    sol.remove_max(a, b)
+    assert a == [1]
+    assert b == [-68, 10]
+    sol.remove_max(a, b)
+    assert (a, b) == ([1], [-68])
+
+
 def test_find_median_two_arrays_small():
     sol = median.Solution()
     assert sol.findMedianSortedArrays([1, 3], [2]) == 2
@@ -31,6 +229,81 @@ def test_two_arrays_mixed():
     a = [-1, 9, 10]
     b = [0, 1, 7]
     expected_median = statistics.median(a + b)
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_two_arrays_mixed_2():
+    sol = median.Solution()
+    a = [7, 21, 500]
+    b = [1, 5, 7]
+    expected_median = statistics.median(a + b)
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_two_odd_arrays():
+    sol = median.Solution()
+    a = [9, 10, 20]
+    b = [1, 7, 100]
+    expected_median = statistics.median(a + b)  # 9.5
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_two_arrays_size_four():
+    sol = median.Solution()
+    a = [-1, 9, 10, 20]
+    b = [0, 1, 7, 100]
+    expected_median = statistics.median(a + b)  # 8.0
+    sol_median = sol.findMedianSortedArrays(a, b)
+
+    assert sol_median == expected_median
+
+
+def test_two_arrays_mixed_larger():
+    sol = median.Solution()
+    a = [-1, 6, 7, 21, 500]
+    b = [0, 0.5, 1, 5, 7]
+    expected_median = statistics.median(a + b)
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_one_singleton_one_large():
+    sol = median.Solution()
+    a = [-1]
+    b = [-50, 0, 1, 7, 65, 100]
+    expected_median = statistics.median(a + b)
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_very_unbalanced():
+    sol = median.Solution()
+    a = [-1, 10, 50]
+    b = [-200, -50, 0, 1, 7, 65, 100, 120, 121, 122, 156, 569, 603]
+    # Merged is [-200, -50, -1, 0, 1, 7, 10, 50, 65, 100, 120, 121, 122, 156, 569, 603]
+    expected_median = statistics.median(a + b)
+    sol_median = sol.findMedianSortedArrays(a, b)
+    assert sol_median == expected_median
+
+
+def test_two_arrays_same():
+    sol = median.Solution()
+    a = [1, 2, 3]
+    b = [1, 2, 3]
+    expected_median = statistics.median(a + b)
+
+    assert sol.findMedianSortedArrays(a, b) == expected_median
+
+
+def test_two_arrays_same_median():
+    sol = median.Solution()
+    a = [-100, 2, 302]
+    b = [1, 2, 50]
+    expected_median = statistics.median(a + b)
+
     assert sol.findMedianSortedArrays(a, b) == expected_median
 
 
@@ -53,14 +326,42 @@ def test_one_missing():
     assert sol.findMedianSortedArrays([], [5]) == 5
 
 
+def test_merge():
+    sol = median.Solution()
+    a = [0, 2]
+    b = [-1, 1]
+    expected_merge = sorted(a + b)
+    actual = sol.merge(a, b)
+    assert actual == expected_merge
+
+
+def test_from_random():
+    sol = median.Solution()
+    a = [-10, -6, -4, -3, 0, 2, 4, 7, 8, 9]
+    b = [-7, -5, -4, -2, -1, 1, 3, 6, 7, 8]
+    expected_median = statistics.median(a + b)
+    assert sol.findMedianSortedArrays(a, b) == expected_median
+
+
 def test_randoms():
     sol = median.Solution()
     million = 1000000
-    list_of_randoms1 = random.sample(range(-million, million), 50)
-    list_of_randoms2 = random.sample(range(-million, million), 50)
+    list_of_randoms1 = random.sample(range(-10, 10), 10)
+    list_of_randoms2 = random.sample(range(-10, 10), 10)
     median_of_both = statistics.median(list_of_randoms1 + list_of_randoms2)
 
     list_of_randoms1.sort()
     list_of_randoms2.sort()
 
-    assert sol.findMedianSortedArrays(list_of_randoms1, list_of_randoms2) == median_of_both
+    sol_median = sol.findMedianSortedArrays(list_of_randoms1, list_of_randoms2)
+
+    passed = sol_median == median_of_both
+
+    if not passed:
+        print("\n")
+        print(list_of_randoms1)
+        print(list_of_randoms2)
+        print("Real median: {}".format(median_of_both))
+        print("Found median: {}".format(sol_median))
+
+    assert passed
