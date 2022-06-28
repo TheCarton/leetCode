dd234bfbe64b4faf5e4c32acee888e04e759d44c
Luke <vandermale.l@northeastern.edu>
2022-06-27 22:10:22 -0400

    python 3 dammit

--- first_missing_positive/first_missing_positive.py
+++ first_missing_positive/first_missing_positive.py
@@ -44,13 +44,11 @@ class Solution(object):
         nums.append(0)
         n = len(nums)
         for i in range(n):
-            if nums[i] < 1 or nums[i] >= n:
+            if nums[i] < 0 or nums[i] >= n:
                 nums[i] = 0
         for i in range(n):
             nums[nums[i] % n] += n
-        pos = n
         for i in range(1, n):
-            if nums[i] / n == 0:
-                pos = i
-                break
-        return pos
+            if nums[i] // n == 0:
+                return i
+        return n
